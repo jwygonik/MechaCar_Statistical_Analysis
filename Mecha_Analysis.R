39d898b1edb46611d7da1e97e09966199b04adbe
jwygonik <97460158+jwygonik@users.noreply.github.com>
2022-07-03 00:55:01 -0400

    Linear Regression for Deliverable 1

---
+++ Mecha_Analysis.R
@@ -0,0 +1,13 @@
+# DELIVERABLE 1 - Linear Regression to Predict MPG
+
+## Load Tidyverse/dplyr package
+library(tidyverse)
+
+## Import and read CSV 
+mpg_data <- read.csv('MechaCar_mpg.csv', stringsAsFactors = F, check.names = F)
+
+## Execute Linear Regression using lm() function - pass all 6 variables 
+mpg_lreg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data = mpg_data)
+
+## Use summary() to get the p-value and r-squared of linreg 
+summary(mpg_lreg)
