743ad228807b900395ca0a7249ba93441c8bd1a6
jwygonik <97460158+jwygonik@users.noreply.github.com>
2022-07-03 01:37:15 -0400

    Deliverable 2 R script added

--- Mecha_Analysis.R
+++ Mecha_Analysis.R
@@ -12,3 +12,19 @@ mpg_lreg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cl
 ## Use summary() to get the p-value and r-squared of linreg 
 summary(mpg_lreg)
 
+# DELIVERABLE 2 - Visualizations for Trip Analysis
+
+## Import and read csv 
+sus_data <- read.csv('Suspension_Coil.csv', stringsAsFactors = F, check.names = F)
+
+## Generate total_summary dataframe using summarize()
+  ### get mean, median, variance, st dev of suspension PSI columns
+total_summary <- sus_data %>% 
+  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+## Check results
+total_summary
+
+## Generate lot_summary df using summarize and group_by
+lot_summary <- sus_data %>% group_by(Manufacturing_Lot) %>% 
+  summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
